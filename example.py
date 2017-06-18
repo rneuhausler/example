4cf8adaa051c4e12f028b3bcbe63abce02aedebd
Martin Robinson <martinjrobins@gmail.com>
2017-06-18 06:36:44 +0100

    added example.py

---
+++ example.py
@@ -0,0 +1,35 @@
+import sparpy
+import random
+
+def test_exponential_force():
+    N = 100
+    D = 0.001
+    lower_bound = [0,0]
+    upper_bound = [1,1]
+    periodic = [True,True]
+    sim_time = 10.0
+    number_of_observations = 100
+    integrate_time = sim_time/number_of_observations
+    dt = 0.001
+    epsilon = 0.01
+    cutoff = 0.1
+
+    particles = sparpy.Particles2(N)
+    for p in particles:
+        p.position = [random.uniform(lower_bound[0],upper_bound[0]),
+                      random.uniform(lower_bound[1],upper_bound[1])]
+
+    simulation = sparpy.Simulation2()
+    simulation.set_domain(lower_bound,upper_bound,periodic)
+    simulation.add_particles(particles,D)
+    simulation.add_force(particles,particles,sparpy.exponential_force2(cutoff,epsilon))
+
+    for i in range(number_of_observations):
+        simulation.integrate(integrate_time,dt)
+
+    assert len(particles) == N
+
+
+if __name__ == "__main__":
+    test_exponential_force()
+
