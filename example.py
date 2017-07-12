3c82d379d95ec77e3fe313497199803269fd0bcd
Rosanna Neuhausler <rneuhausler@berkeley.edu>
2017-07-12 15:43:52 +0100

    grid setup

--- example.py
+++ example.py
@@ -3,32 +3,55 @@ import random
 
 
 def test_exponential_force():
-    N = 100
-    D = 0.00001
+    N = 20
+    D = 0.1
     lower_bound = [0, 0]
-    upper_bound = [1, 1]
-    periodic = [True, True]
-    sim_time = 10.0
+    upper_bound = [25, 25]
+    periodic = [False, False]
+    sim_time = 100.0
     number_of_observations = 100
     integrate_time = sim_time / number_of_observations
-    dt = 0.001
-    epsilon = 0.000001
-    cutoff = 1.0
+    dt = 0.01
+    epsilon = 10000
+    cutoff = 20
+    
+    lower_grid  = 2.5
+    upper_grid  = 22.5
+    step_grid  = 5
+    grid  = [lower_grid + x*step_grid for x in range(int((upper_grid-lower_grid)/step_grid)+1)]
 
+    number_coral = 1 
+    number_turf = 23
+    number_macroalgae = 1
+       
+    fixed = sparpy.Particles2(len(grid) ** 2)
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            fixed[count].position = [grid[i],grid[j]]
+            fixed[count].velocity = [0,0]
+            if count < number_coral:
+                fixed[count].species = 0
+            elif count < number_coral + number_turf:
+                fixed[count].species = 1
+            else:
+                fixed[count].species = 2
+            count = count+1
+    
     particles = sparpy.Particles2(N)
-    fixed = sparpy.Particles2(1)
-    fixed[0].position = [0.1,0.1]
-    fixed[0].species = 0
-    fixed[0].velocity = [0,0]
+    
     for p in particles:
         p.position = [random.uniform(lower_bound[0], upper_bound[0]),
                       random.uniform(lower_bound[1], upper_bound[1])]
+        p.velocity = [0,0]
+        p.species = 0
+        p.force = [0,0]
 
 
     simulation = sparpy.Simulation2()
     simulation.set_domain(lower_bound, upper_bound, periodic)
     simulation.add_particles(particles, D)
-    simulation.add_particles(fixed, D/100.0)
+    simulation.add_particles(fixed, 0)
     simulation.add_force(particles, fixed, sparpy.exponential_force2(cutoff, epsilon))
 
     for i in range(number_of_observations):
