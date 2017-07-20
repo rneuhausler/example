f770f5b0ca86ac371900df5421daf5d9d8bb04f5
Rosanna Neuhausler <rneuhausler@berkeley.edu>
2017-07-20 10:41:04 +0100

    updating grid

--- example.py
+++ example.py
@@ -7,30 +7,34 @@ def test_exponential_force():
     lower_bound = [0, 0]
     upper_bound = [25, 25]
     periodic = [True, True]
+    
     sim_time = 10000
     number_of_observations = 100
     integrate_time = sim_time / number_of_observations
     dt = 0.01
     
+    grid_dt = 100
+    grid_updates_per_observation = int(integrate_time / grid_dt)
+    
     #lr and la cannot be zero (in the denominator)
-    C = {'cutoff':25, 'Ca':1, 'la':5, 'Cr':0, 'lr':0.1, 'type':0,'percent':37} 
+    C = {'cutoff':25, 'Ca':1, 'la':5, 'Cr':0, 'lr':0.1, 'type':0,'percent':37}
     M = {'cutoff':25, 'Ca':1, 'la':5, 'Cr':0, 'lr':0.1, 'type':1, 'percent':37}
     T = {'cutoff':10, 'Ca':0, 'la':0.1, 'Cr':1, 'lr':4, 'type':2, 'percent':26}
     F = {'cutoff':6, 'Ca':0.05, 'la':5, 'Cr':1, 'lr':1, 'type':3, 'count': 20}
-    
 
-    #percent_coral = 60 
+
+    #percent_coral = 60
     #percent_turf = 36
     #percent_macroalgae = 4
-    
+
     lower_grid  = 2.5
     upper_grid  = 22.5
     step_grid  = 5
     grid  = [lower_grid + x*step_grid for x in range(int((upper_grid-lower_grid)/step_grid)+1)]
-    
+
     if C['percent'] + T['percent'] + M['percent'] != 100:
         print "percentages do not add up to 100"
-       
+
     fixed = sparpy.Particles2(len(grid) ** 2)
     count = 0
     for i in range(len(grid)):
@@ -44,8 +48,8 @@ def test_exponential_force():
                 fixed[count].species = 2
             else:
                 fixed[count].species = 1
-            count = count+1 
-                     
+            count = count+1
+
     particles = sparpy.Particles2(F['count'])
     for p in particles:
         p.position = [random.uniform(lower_bound[0], upper_bound[0]),
@@ -67,20 +71,28 @@ def test_exponential_force():
     simulation.add_force(particles, fixed, sparpy.morse_force2(M['cutoff'], M['Ca'], M['la'], M['Cr'], M['lr'], M['type']))
     simulation.add_force(particles, particles, sparpy.morse_force2(F['cutoff'], F['Ca'], F['la'], F['Cr'], F['lr'], F['type']))
 
-    
+
     simulation.add_action(fixed, particles, sparpy.calculate_density2(2.5,dt))
     simulation.add_action(fixed, fixed, sparpy.calculate_density2(6,dt))
 
 
-    
-    
+
+
     #    simulation.add_force(particles, particles, sparpy.exponential_force2(cutoff, epsilon))
 
 
+        
     for i in range(number_of_observations):
-        for p in fixed:
-            p.density = [0.0,0.0,0.0,0.0]
-        simulation.integrate(integrate_time, dt)
+        for j in range(grid_updates_per_observation):
+            for p in fixed:
+                p.density = [0.0,0.0,0.0,0.0]
+            simulation.integrate(grid_dt, dt)
+            for p in fixed:
+                if p.density[3] > 0:
+                    p.species = C['type']
+        #simulation.update_grid()
+          #print particles[0].force
+
         #simulation.update_grid()
         #print particles[0].force
 
@@ -97,12 +109,46 @@ def test_exponential_force():
 if __name__ == "__main__":
     test_exponential_force()
 
+"""
+    sim_time = 10000
+    number_of_observations = 100
+    integrate_time = sim_time / number_of_observations
+    dt = 0.01
+"""
+
+
+"""
+
+    for i in range(number_of_observations):
+        for p in fixed:
+            p.density = [0.0,0.0,0.0,0.0]
+        simulation.integrate(integrate_time, dt)
+
+
+
+    for i in range(number_of_observations):
+        for j in range(grid_updates_per_observation):
+            for p in fixed:
+                p.density = [0.0,0.0,0.0,0.0]
+                simulation.integrate(grid_dt, dt)
+            for p in fixed:
+                if p.density[3] > 0:
+                    p.species = C['type']
+"""
+
+
+
 
 """ Block grid species assignment
-    number_coral = 1 
+
+
+
+
+    number_coral = 1
     number_turf = 23
     number_macroalgae = 1
-       
+
+
     fixed = sparpy.Particles2(len(grid) ** 2)
     count = 0
     for i in range(len(grid)):
